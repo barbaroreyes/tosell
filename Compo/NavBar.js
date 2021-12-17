6ad0a6e57a450f1dcf3343f80061ac13cf4c47d1
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-17 11:37:04 -0600

    update-nav

--- Compo/NavBar.js
+++ Compo/NavBar.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import styles from '../styles/Navbar.module.css'
+import Image from 'next/image'
 const NavBar = () => {
   return (
-    <div>
-      NavBar
+    <div className = {styles.container}>
+      <div className = {styles.item}>
+      <div className = {styles.callButton}>
+         <Image src='/img/cellPhone.jpeg' alt="" width="62" height="62"/>
+        </div>
+        <div className = {styles.texts}>
+        <div className = {styles.text}>Order Now </div>
+        <div className = {styles.text}>Order 123 342 2432 </div>
+        </div>
+
+
+      </div>
+      <div className = {styles.item}></div>
+      <div className = {styles.item}></div>
     </div>
   )
 }
