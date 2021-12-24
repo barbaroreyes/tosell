bc6246bd8ca2f4f83d465c719dc51681bb951e74
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-24 17:43:29 -0600

    update

--- Compo/Feature.js
+++ Compo/Feature.js
@@ -1,9 +1,34 @@
 import React from 'react'
-import styles from '../styles/Featured.module.css'
+import styles from '../styles/Featured.module.css';
+import Image from  'next/image'
 const Featured= () => {
+    const images = [
+        '/img/fondo-de-pantalla.jpeg',
+        '/img/gargantilla.jpeg',
+        '/img/Argolla.jpeg'
+
+    ]
   return (
     <div className ={styles.container}>
-       Featured    
+        <div className ={styles.arrowContainer}>
+        <Image src='/img/arrowright.png' alt ='' width='32' height='32' /> 
+        </div>
+    
+       <div className ={styles.wraper}>
+           <div className ={styles.imageContainer}>
+            {images.map((item,i)=>{
+                return (
+                    <Image key={i} src={item} alt ='' layout='fill'/> 
+                )
+            })}
+           </div>
+
+       </div>
+       <div className ={styles.arrowContainer}>
+       <Image src='/img/arrowleft-2.png' alt ='' layout='fill'/> 
+
+       </div>
+       
       </div>
   )
 }
